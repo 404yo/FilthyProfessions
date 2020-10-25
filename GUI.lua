51430cc939257bb35bd130a13c05a6ceb94d0e35
Jeger Ibrahim <jeger.ibrahim.sf@gmail.com>
2020-10-26 00:14:44 +0100

    some minor fixes

--- GUI.lua
+++ GUI.lua
@@ -2,6 +2,15 @@ local db = _G.gDB
 local GUI = {}
 local GUI_INIT = false
 
+
+
+
+function GUI:init()
+
+    GUI:Create()
+
+end
+
 function GUI:Create() 
     if GUI_INIT then return end
     GUI_INIT = true
@@ -21,10 +30,31 @@ function GUI:Create()
     frame:SetClampedToScreen(true)
     frame:SetBackdropColor(0.45,0.45,0.45,1)
     frame:Hide()
+    tinsert(UISpecialFrames, frameName)
 
 
-    frame.CloseButton = CreateFrame("Button", frameName.."-CloseButton", frame, "UIPanelCloseButton")
-    frame.CloseButton:SetPoint("TOPRIGHT", frame, "TOPRIGHT", 0, 0)
+    frame.CloseButton = CreateFrame("Button", frameName.."_CLOSE", frame, "UIPanelCloseButton")
+    frame.CloseButton:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -2, -2)
+
+
+    frame.menuFrame = CreateFrame("Frame", frameName.."_MENU", frame)
+    frame.menuFrame:ClearAllPoints()
+    frame.menuFrame:setParent(frame)
+    frame.menuFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", 5,-5)
+    frame.menuFrame:SetWidth(frame:GetWidth()/4)
+    frame.menuFrame:setHeight(frame:GetHeight()-10)
+
+    frame.menuFrame.titleFrame = CreateFrame()
+
+
+
+    frame.contentFrame = CreateFrame("Frame", frameName .."-main-content",frame)
+    frame.contentFrame:ClearAllPoints()
+	frame.contentFrame:SetParent(frame)
+	frame.contentFrame:SetPoint("TOPLEFT", frame, "TOPLEFT", 10, -70)
+	frame.contentFrame:SetWidth(560)		-- Frame = 560, Abstand = 20, Button = 270
+	frame.contentFrame:SetHeight(510)		-- Frame = 460, Abstand = 10, Button = 30
+	frame.contentFrame.shownFrame = nil
     
     
 end
