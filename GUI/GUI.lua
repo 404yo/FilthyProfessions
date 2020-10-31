216f8a00825e624e8ba2d651c8a341b7f26a6c77
Jeger Ibrahim <jeger.ibrahim.sf@gmail.com>
2020-10-31 13:03:29 +0100

    remade the layout a bit

--- GUI/GUI.lua
+++ GUI/GUI.lua
@@ -26,9 +26,16 @@ function GUI:init()
  
 end
 
+function UIMenuButton_OnLoad()
+	-- this:RegisterForClicks("LeftButtonUp", "RightButtonUp");
+	-- this:RegisterEvent("UNIT_QUEST_LOG_CHANGED");
+	-- this:RegisterEvent("PARTY_MEMBER_ENABLE");
+	-- this:RegisterEvent("PARTY_MEMBER_DISABLE");
+end
+
+
 function GUI:reloadDB()
     gDB  = _G.DB
- 
     AlchemyDB = gDB["Alchemy"] or {}
     EnchantingDB = gDB["Enchanting"] or {}
     BlacksmithingDB = gDB["Black Smithing"] or {}
@@ -44,82 +51,87 @@ function GUI:refresh()
 end
 
 function GUI:CreateMainFrame(frameName)
-
-    local frame = CreateFrame("Frame", frameName, UIParent, "BasicFrameTemplateWithInset")
+    -- "BasicFrameTemplateWithInset"
+    local frame = CreateFrame("Frame", frameName, UIParent)
     frame:ClearAllPoints()
-    frame:SetPoint("CENTER", UiParent, "CENTER")
-    frame:SetWidth(920)
+    frame:SetPoint("CENTER", UIParent, "CENTER")
+    frame:SetWidth(900)
     frame:SetHeight(600)
     frame:SetMovable(true)
     frame:EnableMouse(true)
     frame:RegisterForDrag("LeftButton", "RightButton")
     frame:SetToplevel(true)
     frame:SetClampedToScreen(true)
-    frame.TitleText:SetText("GuildProfessions")
+    -- frame.TitleText:SetText("GuildProfessions")
     frame:Show()
     return frame
 end
 
-function GUI:CreateItemFrame(frameName, parent,itemData)
-    local rowFrame = CreateFrame("Frame", frameName, parent, "InsetFrameTemplate")
-    rowFrame:SetWidth(parent:GetWidth())
-    rowFrame:SetHeight(parent:GetHeight() / 20)
-    rowFrame:SetPoint("TOP", parent, "TOP")
-    local t = rowFrame:CreateFontString(rowFrame,"OVERLAY","GAMETOOLTIPTEXT")
-    t:SetPoint("LEFT",20,0)
-    t:SetText(itemData[0])
+function GUI:CreateItemButtonFrame(frameName, parent,itemData)
+    local rowFrame = CreateFrame("Button", frameName, parent, "InsetFrameTemplate2")
+    rowFrame:SetWidth((parent:GetWidth()-4))
+    rowFrame:SetHighlightTexture("Interface\\QuestFrame\\UI-QuestTitleHighlight", "ADD")
+    rowFrame:SetHeight((parent:GetHeight()-4) / 22)
+    rowFrame:SetPoint("TOP", parent, "TOP",0,0)
+    rowFrame.text = rowFrame:CreateFontString(rowFrame,"OVERLAY","GAMETOOLTIPTEXT")
+    rowFrame.text:SetPoint("LEFT",20,0)
+    rowFrame.text:SetText(itemData[0])
 
-    local first = true
-    local playerText = {}
-    for k,v in pairs(itemData[1]) do
-        playerText[k] = rowFrame:CreateFontString(rowFrame,"OVERLAY","GAMETOOLTIPTEXT")
-        playerText[k]:SetText(v)
 
-        if first then 
-            playerText[k]:SetPoint("RIGHT",rowFrame,"CENTER",-40,0)
-        else 
-            playerText[k]:SetPoint("LEFT",playerText[k-1],"RIGHT",-40,0)
-        end    
-       first = false
-    end
 
-    first = true
-    local reagentText = {}
-    local reagentString
-    local i = 0
-    for k,v in pairs(itemData[2]) do
-        print(tostring(k),tostring(v))
-        reagentText[i] = rowFrame:CreateFontString(rowFrame,"OVERLAY","GAMETOOLTIPTEXT")
-        reagentText[i]:SetText(tostring(k)  .. " x " ..v)
-        if first then 
-            reagentText[i]:SetPoint("LEFT",rowFrame,"CENTER",0,0)
-        else 
-            reagentText[i]:SetPoint("LEFT",reagentText[i-1],"RIGHT",10,0)
-        end  
-        i = i + 1
-        first = false
 
-    end
+
+    -- local first = true
+    -- local playerText = {}
+    -- for k,v in pairs(itemData[1]) do
+    --     playerText[k] = rowFrame:CreateFontString(rowFrame,"OVERLAY","GAMETOOLTIPTEXT")
+    --     playerText[k]:SetText(v)
+
+    --     if first then 
+    --         playerText[k]:SetPoint("RIGHT",rowFrame,"CENTER",-40,0)
+    --     else 
+    --         playerText[k]:SetPoint("LEFT",playerText[k-1],"RIGHT",-40,0)
+    --     end    
+    --    first = false
+    -- end
+
+    -- first = true
+    -- local reagentText = {}
+    -- local reagentString
+    -- local i = 0
+    -- for k,v in pairs(itemData[2]) do
+    --     print(tostring(k),tostring(v))
+    --     reagentText[i] = rowFrame:CreateFontString(rowFrame,"OVERLAY","GAMETOOLTIPTEXT")
+    --     reagentText[i]:SetText(tostring(k)  .. " x " ..v)
+    --     if first then 
+    --         reagentText[i]:SetPoint("LEFT",rowFrame,"CENTER",0,0)
+    --     else 
+    --         reagentText[i]:SetPoint("LEFT",reagentText[i-1],"RIGHT",10,0)
+    --     end  
+    --     i = i + 1
+    --     first = false
+
+    -- end
 
     
     return rowFrame
 end
 
-function GUI:CreateScrollFrame(frameName,parent,child, relativeFrame) 
+function GUI:CreateScrollFrame(frameName,parent,child) 
     local scrollFrame =  CreateFrame ("ScrollFrame", frameName, parent, "UIPanelScrollFrameTemplate")
-    scrollFrame:SetWidth(parent:GetWidth() - relativeFrame:GetWidth() - 17)
-    scrollFrame:SetHeight(relativeFrame:GetHeight())
-    scrollFrame:SetPoint("LEFT", relativeFrame, "RIGHT")
+    scrollFrame:SetWidth(child:GetWidth())
+    scrollFrame:SetHeight(child:GetHeight())
+    scrollFrame:SetPoint("BOTTOM", parent, "BOTTOM",-12,5)
     scrollFrame:SetScrollChild(child)
     return scrollFrame
 
 end
 
-function GUI:CreateContentFrame(frameName, parent, relativeFrame)
+function GUI:CreateContentFrame(frameName, parent)
     local contentFrame = CreateFrame("Frame", frameName, parent, "InsetFrameTemplate2")
-    contentFrame:SetWidth(parent:GetWidth() - relativeFrame:GetWidth() - 17)
-    contentFrame:SetHeight(relativeFrame:GetHeight())
-    contentFrame:SetPoint("LEFT", relativeFrame, "RIGHT")
+    contentFrame:SetWidth(parent:GetWidth()-40)
+    contentFrame:SetHeight(parent:GetHeight()-100)
+    contentFrame:SetPoint("BOTTOM", parent, "BOTTOM",-12,5)
     return contentFrame
 end
 
@@ -149,12 +161,33 @@ function GUI:Create()
     GUI_INIT = true
     local frameName = "MAIN_FRAME"
     GUI.frame = GUI:CreateMainFrame(frameName)
-    local menuName = "LEFT_MENU"
-    GUI.menu = GUI:CreateMenuFrame(menuName, GUI.frame)
+    -- local menuName = "LEFT_MENU"
+    -- GUI.menu = GUI:CreateMenuFrame(menuName, GUI.frame)
+    
+    GUI.parentItemFrame = CreateFrame("Frame","MAIN_ITEM_FRAME",GUI.frame,"BasicFrameTemplateWithInset")
+    GUI.parentItemFrame:SetWidth(GUI.frame:GetWidth()/3+80)
+    GUI.parentItemFrame:SetHeight(GUI.frame:GetHeight())
+    GUI.parentItemFrame:SetPoint("LEFT", GUI.frame, "LEFT")
+    
+    GUI.itemFilterMenu = CreateFrame("Frame","ITEM_FILTER_MENU", GUI.parentItemFrame,"InsetFrameTemplate2")
+    GUI.itemFilterMenu:SetHeight(GUI.parentItemFrame:GetHeight()/6)
+    GUI.itemFilterMenu:SetWidth(GUI.parentItemFrame:GetWidth()-10)
+    GUI.itemFilterMenu:SetPoint("TOP",GUI.parentItemFrame,"TOP",0,-20)
+
+
     local contenFrameName = "CONTENT_FRAME"
-    GUI.content = GUI:CreateContentFrame(contenFrameName, GUI.frame, GUI.menu)
+    GUI.content = GUI:CreateContentFrame(contenFrameName,GUI.parentItemFrame)
+    GUI.content:SetHeight(GUI.parentItemFrame:GetHeight()-GUI.itemFilterMenu:GetHeight())
+    GUI.content:SetWidth(GUI.parentItemFrame:GetWidth()-40)
+    GUI.content:SetPoint("TOP",GUI.itemFilterMenu,"BOTTOM",-15,0)
+
+    
     local scrollFrameName = "ITEM_SCROLL_FRAME"
-    local scrollFrame = GUI:CreateScrollFrame(scrollFrameName, GUI.frame,GUI.content, GUI.menu)
+    GUI.scrollFrame = GUI:CreateScrollFrame(scrollFrameName, GUI.parentItemFrame,GUI.content)
+    GUI.scrollFrame:SetHeight(GUI.parentItemFrame:GetHeight()-GUI.itemFilterMenu:GetHeight())
+    GUI.scrollFrame:SetWidth(GUI.parentItemFrame:GetWidth()-40)
+    GUI.scrollFrame:SetPoint("TOP",GUI.itemFilterMenu,"BOTTOM",-15,0)
+
 
     ----------------PROFESSION CHECKBOXES--------------------
     local alchyMyName = "ALCHEMY_BOX"
@@ -169,7 +202,7 @@ function GUI:Create()
     GUI.enchantingBox = GUI:CreateCheckBox(enchantingName, GUI.menu, "Enchanting", true)
     GUI.enchantingBox:SetPoint("TOP", GUI.blacksmithingBox, "BOTTOM", 0, -5)
 
-    local engineeringName = "ENGINEERING_BOX"
+    local engineeringName = "ENGINEERING_BOX"   
     GUI.engineeringBox = GUI:CreateCheckBox(enchantingName, GUI.menu, "Enchanting", true)
     GUI.engineeringBox:SetPoint("TOP", GUI.enchantingBox, "BOTTOM", 0, -5)
 
@@ -213,6 +246,14 @@ function GUI:FillContent()
     end
 
     for k,v in pairs(FirstAidDB) do
+        table.insert(items,GUI:ParseDBItem(k,v))
+    end
+
+    for k,v in pairs(EnchantingDB) do
+        table.insert(items,GUI:ParseDBItem(k,v))
+    end
+
+    for k,v in pairs(CookingDB) do
         table.insert(items,GUI:ParseDBItem(k,v))
     end
     
